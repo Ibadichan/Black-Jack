4d2037de5b61968fd7d06f9e9a2ecd2523b80488
Badicean Ivan <badican01117@gmail.com>
2017-04-09 15:47:17 +0200

    black-jack errors

---
+++ dealer.rb
@@ -0,0 +1,10 @@
+class Dealer
+  attr_accessor :bank, :cards, :methods
+
+  def initialize
+    @bank = 100
+    @cards =[]
+    @methods = %w(skip_a_move add_card open_all_cards)
+  end
+
+end
--- player.rb
+++ player.rb
@@ -1,10 +1,10 @@
-class ARM
+class Player
   attr_accessor :bank, :cards, :methods
 
   def initialize
     @bank = 100
     @cards =[]
-    @methods = ['skip_a_move','add_card','open_all_cards']
+    @methods = %w(skip_a_move add_card open_all_cards)
   end
 
 end
